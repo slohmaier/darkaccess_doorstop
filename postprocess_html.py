f471deb85a81dca2aa74a45423f018b33fa22303
Stefan Lohmaier <stefan@slohmaier.de>
2026-02-08 09:36:46 +0100

    Fix Bootstrap CSS SRI integrity hash

--- postprocess_html.py
+++ postprocess_html.py
@@ -21,7 +21,7 @@ import sys
 BOOTSTRAP_CSS_CDN = (
     '<link rel="stylesheet" '
     'href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" '
-    'integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YcnS/S7cOl+1QRLI0jaPJSKyIp6s0GOuQ24p" '
+    'integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" '
     'crossorigin="anonymous">'
 )
 
